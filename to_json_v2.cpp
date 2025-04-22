c13ca8895200289d8843d7ea83f8ed80543da4e9
Annatar <stijnjongbloed@hotmail.com>
2025-04-22 17:07:17 +0200

    DecayTree is functional on my main PC atleast, Laptop remains to be tested.

---
+++ to_json_v2.cpp
@@ -0,0 +1,156 @@
+// Used https://github.com/nlohmann/json for the json writing.
+//
+// ./simpleRNTuple  35.33s user 50.88s system 99% cpu 1:26.83 total according to
+// time on my machine. For DecayTree that is.
+
+#include <ROOT/RNTupleReader.hxx>
+
+#include <ROOT/RNTupleUtil.hxx>
+#include <ROOT/RNTupleView.hxx>
+#include <TCanvas.h>
+#include <TH1I.h>
+#include <TROOT.h>
+#include <TString.h>
+
+#include <cassert>
+#include <cstdint>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <ostream>
+#include <string>
+#include <string_view>
+#include <utility>
+#include <variant>
+#include <vector>
+using json = nlohmann::json;
+
+using FVec = std::variant<std::vector<std::string>, std::vector<std::int32_t>,
+                          std::vector<std::uint32_t>, std::vector<double>>;
+using VVec =
+    std::variant<ROOT::RNTupleView<std::string>,
+                 ROOT::RNTupleView<std::int32_t>,
+                 ROOT::RNTupleView<std::uint32_t>, ROOT::RNTupleView<double>>;
+
+std::vector<std::pair<std::string, std::string>>
+GetFieldNamesAndTypes(const ROOT::REntry &entry) {
+  std::vector<std::pair<std::string, std::string>> fields;
+
+  for (const auto &val : entry) {
+    fields.emplace_back(val.GetField().GetFieldName(),
+                        val.GetField().GetTypeName());
+  }
+
+  return fields;
+}
+
+template <typename T>
+inline void ProcessNumberField(std::ostream &out, const std::string &fieldName,
+                               ROOT::RNTupleView<T> &view,
+                               std::uint64_t entryId) {
+  out << '"' << fieldName << "\":" << '"' << view(entryId) << '"';
+}
+
+inline void ProcessStringField(std::ostream &out, std::string_view fieldName,
+                               ROOT::RNTupleView<std::string> &view,
+                               std::uint64_t entryId) {
+  out << '"' << fieldName << "\":\"" << view(entryId) << '"';
+}
+
+enum FieldTypes { String, Int32, Uint32, Double };
+
+// constexpr char const *kNTupleFileName = "ntpl001_staff.root";
+constexpr char const *kNTupleFileName = "B2HHH.ntuple.root";
+// constexpr char const *kNTupleFileName = "ntpl002_vector.root";
+constexpr char const *JSONName = "out.ndjson";
+
+int main() {
+  // auto reader = ROOT::RNTupleReader::Open("Staff", kNTupleFileName);
+  // auto reader = RNTupleReader::Open("F", kNTupleFileName);
+  auto reader = ROOT::RNTupleReader::Open("DecayTree", "B2HHH.ntuple.root");
+  auto fields = GetFieldNamesAndTypes(reader->GetModel().GetDefaultEntry());
+  std::ofstream file(JSONName);
+  std::ostream &output = file;
+
+  std::vector<std::pair<std::string, int>> fieldMap;
+  std::vector<FVec> fieldsVec;
+  std::vector<VVec> viewVec;
+  std::cout << "Initializing arrays…" << std::endl;
+
+  for (const auto &[fieldName, fieldType] : fields) {
+    if (fieldType == "std::string") {
+      fieldsVec.emplace_back(std::in_place_type<std::vector<std::string>>,
+                             reader->GetNEntries());
+      viewVec.emplace_back(reader->GetView<std::string>(fieldName));
+      fieldMap.emplace_back(fieldName, FieldTypes::String);
+    } else if (fieldType == "std::int32_t") {
+      fieldsVec.emplace_back(std::in_place_type<std::vector<std::int32_t>>,
+                             reader->GetNEntries());
+      viewVec.emplace_back(reader->GetView<std::int32_t>(fieldName));
+      fieldMap.emplace_back(fieldName, FieldTypes::Int32);
+    } else if (fieldType == "std::uint32_t") {
+      fieldsVec.emplace_back(std::in_place_type<std::vector<std::uint32_t>>,
+                             reader->GetNEntries());
+      viewVec.emplace_back(reader->GetView<std::uint32_t>(fieldName));
+      fieldMap.emplace_back(fieldName, FieldTypes::Uint32);
+    } else if (fieldType == "double") {
+      fieldsVec.emplace_back(std::in_place_type<std::vector<double>>,
+                             reader->GetNEntries());
+      viewVec.emplace_back(reader->GetView<double>(fieldName));
+      fieldMap.emplace_back(fieldName, FieldTypes::Double);
+    } else {
+      std::cerr << "Found an unsupported fieldtype: " << fieldType << std::endl;
+      exit(1);
+    }
+  }
+
+  std::cout << "Reading RNTuple into memory…" << std::endl;
+
+  for (size_t field = 0; field < fieldsVec.size(); ++field) {
+    const auto &[fieldName, fieldType] = fields[field];
+    std::visit(
+        [&](auto &dstVec, auto &srcView) {
+          using D = std::decay_t<decltype(dstVec)>;
+          using Elem =
+              std::remove_const_t<std::remove_reference_t<decltype(srcView(
+                  (ROOT::NTupleSize_t)0))>>;
+          if constexpr (std::is_same_v<typename D::value_type, Elem>) {
+            for (size_t i = 0; i < reader->GetNEntries(); ++i) {
+              dstVec[i] = srcView(static_cast<ROOT::NTupleSize_t>(i));
+            }
+          }
+        },
+        fieldsVec[field], viewVec[field]);
+  }
+
+  std::cout << "Converting to JSON…" << std::endl;
+
+  std::vector<std::string> fieldNames;
+  for (const auto &[fieldName, fieldType] : fields) {
+    fieldNames.emplace_back(fieldName);
+  }
+  json rows = json::array();
+
+  for (size_t i = 0; i < reader->GetNEntries(); ++i) {
+    json obj = json::object();
+    // no obj.reserve(…);
+
+    for (size_t f = 0; f < fieldNames.size(); ++f) {
+      const auto &key = fieldNames[f];
+      std::visit([&](auto &vec) { obj[key] = vec[i]; }, fieldsVec[f]);
+    }
+
+    rows.push_back(std::move(obj));
+  }
+  std::cout << "Writing JSON…" << std::endl;
+
+  std::ofstream ofs("out.json");
+  ofs << rows.dump(2) << "\n";
+
+  std::cout << "Done converting RNTuple to JSON!" << std::endl;
+
+  file.close();
+
+  return 0;
+}
